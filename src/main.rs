f78fb7a9b24f474fb7e9757bb8aba9da2c0c05e8
Achyutha <aachyutha2@gmail.com>
2022-06-15 15:27:53 +0530

    Shares the db among the threads

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 
+use bytes::Bytes;
 use mini_redis::Command::{Get, Set};
 use mini_redis::{Command, Connection, Frame};
 use tokio::net::{TcpListener, TcpStream};
@@ -8,24 +10,30 @@ use tokio::net::{TcpListener, TcpStream};
 async fn main() {
     let listener = TcpListener::bind("0.0.0.0:1234").await.unwrap();
 
+    let db = Arc::new(Mutex::new(HashMap::new()));
+    println!("Listening");
+
     loop {
         let (socket, _) = listener.accept().await.unwrap();
+        let db = db.clone();
+        println!("Accepted");
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, db).await;
         });
     }
 }
 
-async fn process(socket: TcpStream) {
-    let mut db = HashMap::new();
+async fn process(socket: TcpStream, db: Arc<Mutex<HashMap<String, Bytes>>>) {
     let mut connection = Connection::new(socket);
     while let Some(frame) = connection.read_frame().await.unwrap() {
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
             Get(cmd) => {
+                let db = db.lock().unwrap();
                 if let Some(value) = db.get(cmd.key()) {
                     Frame::Bulk(value.clone().into())
                 } else {
