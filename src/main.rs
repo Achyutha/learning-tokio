7e6dc81ba14b99bcd84d64381522e5c63a850a8a
Achyutha <aachyutha2@gmail.com>
2022-06-15 15:17:01 +0530

    Implements Get and Set

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use mini_redis::{Connection, Frame};
+use std::collections::HashMap;
+
+use mini_redis::Command::{Get, Set};
+use mini_redis::{Command, Connection, Frame};
 use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
@@ -14,11 +17,25 @@ async fn main() {
 }
 
 async fn process(socket: TcpStream) {
+    let mut db = HashMap::new();
     let mut connection = Connection::new(socket);
-
-    if let Some(frame) = connection.read_frame().await.unwrap() {
-        println!("GOT {:?}", frame);
-        let response = Frame::Error("Unimplemented".to_string());
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                Frame::Simple("OK".to_string())
+            }
+            Get(cmd) => {
+                if let Some(value) = db.get(cmd.key()) {
+                    Frame::Bulk(value.clone().into())
+                } else {
+                    Frame::Null
+                }
+            }
+            cmd => {
+                panic!("Unimplemented {:?}", cmd);
+            }
+        };
         connection.write_frame(&response).await.unwrap();
     }
 }
